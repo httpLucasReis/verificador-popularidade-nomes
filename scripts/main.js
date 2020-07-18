01475d7b5d3ab60541dd211c378d43861a19d065
httpLucasReis <manoel.reisiema@gmail.com>
2020-07-17 23:49:48 -0300

    Adicionando tabela interativa + refatoração

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
-const result = document.querySelector("#result")
+const result = document.querySelector("#result");
+
+const table = document.createElement('table')
+const tableHead = document.createElement('thead');
+const tableBody = document.createElement('tbody');
 
 document.querySelector("button").addEventListener("click", getNames);
 
-
-
 function getNames(){
     result.innerHTML = '';
+    tableHead.innerHTML = '';
+    tableBody.innerHTML = '';
 
     const name = document.querySelector("input").value;
     
@@ -20,15 +24,6 @@ function getNames(){
 
     const url = `https://servicodados.ibge.gov.br/api/v2/censos/nomes/${name}`;
 
-
-    // Lista que vai receber o período
-    const list_time = document.createElement("ul");
-    list_time.setAttribute("id","time");
-
-    // Lista que vai receber a frequência em que o nome foi registrado
-    const list_quantity = document.createElement("ul")
-    list_quantity.setAttribute("id","quantity")
-
     fetch(url)
       .then((res) => res.json())
       .then((names) => {
@@ -38,18 +33,19 @@ function getNames(){
             let periodo = names[0].res[index].periodo.replace(/(\[)|(\])/g,"");
             let frequencia = names[0].res[index].frequencia;
 
-            // Cria um elemento para cada index do objeto
-            let item_t = document.createElement("li");
-            item_t.textContent = `Período: ${periodo}`;
-
-            let item_q = document.createElement("li");
-            item_q.textContent = `Frequência: ${frequencia}`;
-
-            list_time.appendChild(item_t);
-            list_quantity.appendChild(item_q);
+            tableBody.innerHTML += `<tr><td>${periodo}</td><td>${frequencia}</td></tr>`;
         
         }
-        result.appendChild(list_time);
-        result.appendChild(list_quantity);
+        tableHead.innerHTML = `<tr><th>Período</th> <th>Frequência</th></tr>`;
+        table.appendChild(tableHead);
+        table.appendChild(tableBody);
+        result.appendChild(title)
+        result.appendChild(table);
+      })
+      .catch(function(error){
+        const errorText = document.createElement('h1');
+        const contentText = document.createTextNode(`O nome ${name} é inválido ou não foi encontrado`);
+        errorText.appendChild(contentText);
+        result.appendChild(errorText);
       })
 }
